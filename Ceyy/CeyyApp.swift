89b91540457936f1867c4ec2002c373d16702fcb
ceydasanlikun <sanlikunceyda@gmail.com>
2025-02-10 23:31:04 +0300

    Initial Commit

---
+++ Ceyy/CeyyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CeyyApp.swift
+//  Ceyy
+//
+//  Created by MacBook Air on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CeyyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CeyyTests/CeyyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CeyyTests.swift
+//  CeyyTests
+//
+//  Created by MacBook Air on 10.02.2025.
+//
+
+import Testing
+@testable import Ceyy
+
+struct CeyyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
